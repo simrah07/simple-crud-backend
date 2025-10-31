5174234125bffe26963a03b84b6f7bf88daa8370
Simrah Mahreen <simrahmahreen17@gmail.com>
2025-11-01 00:20:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let items = []
+
+app.get("/items",(req,res) => {
+    res.json(items);
+});
+
+app.post("/items",(req,res) => {
+    const newItem = {id:Date.now(),name:req.body.name};
+    items.push(newItem);
+    res.status(201).json(newItem);
+});
+
+app.patch("/items/:id",(req,res)=>{
+    const item = items.find(i => i.id == req.params.id);
+    if(item){
+        item.name = req.body.name;
+        res.json(item);
+    }else{
+        res.status(404).json({message:"Item not found"});
+    }
+});
+
+app.delete("/items/:id",(req,res)=>{
+    items = items.filter(i => i.id != req.params.id);
+    res.json({message: "item not found"});
+});
+
+const PORT = 5000;
+app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
